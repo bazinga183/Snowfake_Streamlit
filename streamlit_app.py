e10cf96ee7bdd63921312e91fcede6f52ba16fcd
Eduardo Flores <eddie.flores183@gmail.com>
2023-11-23 20:16:56 -0600

    Added a dataframe and multiselect to set up filtering the dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas as pd
 
+#First menu section
 streamlit.title("My Parent's New Healthy Diner!")
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
@@ -8,6 +9,16 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
+#Second menu section
 streamlit.header('🍌🍊Build Your Own Fruit Smoothie🥝🍇')
+
+#Read the csv that houses the information on fruit
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+#Replace the index with the fruit
+my_fruit_list.set_index('Fruit', inplace=True)
+
+#Insert multi-select so customers can pick their fruit(s) of choice
+streamlit.multiselect('Pick your fruits!', list(my_fruit_list.index))
+#Dataframe of available fruits
 streamlit.dataframe(my_fruit_list)
