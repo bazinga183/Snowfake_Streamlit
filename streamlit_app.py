3d916c9cab7dd7097b7ec8ff7519e0d21cbbf5d4
Eduardo Flores <eddie.flores183@gmail.com>
2023-11-23 20:40:19 -0600

    Added the filtering function to the dataframe through multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,8 @@ my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list.set_index('Fruit', inplace=True)
 
 #Insert multi-select so customers can pick their fruit(s) of choice
-streamlit.multiselect('Pick your fruits!', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect('Pick your fruits!', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 #Dataframe of available fruits
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
