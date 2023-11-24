0cfb97601b69a75cc0c92908e0303dc366e14c6a
Eduardo Flores <eddie.flores183@gmail.com>
2023-11-23 20:26:39 -0600

    Added multiselect choices as a test

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,6 @@ my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list.set_index('Fruit', inplace=True)
 
 #Insert multi-select so customers can pick their fruit(s) of choice
-streamlit.multiselect('Pick your fruits!', list(my_fruit_list.index))
+streamlit.multiselect('Pick your fruits!', list(my_fruit_list.index)['Avocado', 'Strawberries'])
 #Dataframe of available fruits
 streamlit.dataframe(my_fruit_list)
