101ea930b0319b19f4d334d27da6c0b50429c910
Eduardo Flores <eddie.flores183@gmail.com>
2023-11-25 16:25:21 -0500

    Added an input fild to allow users to find the fruit they want and a requierments.txt file for a snowflake-python connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,9 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon").json()
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}").json()
 
 #Normalize the json response
 fruityvice_normalized = pd.json_normalize(fruityvice_response) 
