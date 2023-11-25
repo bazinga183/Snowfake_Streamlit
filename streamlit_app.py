726168126c9c85ff0cf3cf55023b05ffa4766185
Eduardo Flores <eddie.flores183@gmail.com>
2023-11-25 16:39:47 -0500

    Added Snowflake secrets and testing its connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,10 @@ fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_cho
 #Normalize the json response
 fruityvice_normalized = pd.json_normalize(fruityvice_response) 
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute('SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()')
+my_data_row = my_cur.fetchone()
+streamlit.text('Hello from Snowflake:')
+streamlit.text(my_data_row)
