93d161210c80f6d0a3b023ae32cbc5202c985ecf
Eduardo Flores <eddie.flores183@gmail.com>
2023-11-25 14:29:55 -0500

    Added a DataFrame of the API call made for a watermelon

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,8 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon").json()
+
+#Normalize the json response
+fruityvice_normalized = pd.json_normalize(fruityvice_response) 
+streamlit.dataframe(fruityvice_normalized)
